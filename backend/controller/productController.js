042898599a31660e0b0b7dbec39dfa3163a1a262
KhubaibAhmadw2 <khubaibahmadw2@gmail.com>
2022-03-12 22:23:04 +0500

    Post request

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,3 +1,18 @@
+const Product=require("../models/productModel");
+
+
+
+//Create Product
+
+exports.createProduct= async(req,res,next)=>{
+    const product=await Product.create(req,body);
+res.status(201).json({
+    success:true,
+    product
+})
+
+}
+
 exports.getAllProducts=(res,req)=>{
     res.status(200).json({message:"route is working fine"})
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,9 @@
 const express =require('express');
-const { getAllProducts } = require('../controller/productController');
+const { getAllProducts,createProduct } = require('../controller/productController');
 
 const router =express.Router();
 
 router.route('./products').get(getAllProducts);
-
+router.route('./product/new').post(createProduct);
 
 module.exports =router 
