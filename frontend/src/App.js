9d01ed4cf606fb75c788b1d3257755b681608118
KhubaibAhmadw2 <khubaibahmadw2@gmail.com>
2022-03-15 00:24:44 +0500

    Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Header from "./component/layout/Header/Header.js";
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router,Route} from "react-router-dom";
 import WebFont from "webfontloader";
 import React from "react";
 import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js";
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
   return (
     <Router>
       <Header/>
+      <Route  exact path="/" component={Home}/>
 
       <Footer/>
     </Router>
