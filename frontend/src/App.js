4b079c2b33c8158cd0b3830688bbc417fd7efc8e
KhubaibAhmadw2 <khubaibahmadw2@gmail.com>
2022-03-14 14:42:14 +0500

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
-
+import Header from "./component/layout/Header.js";
+import {BrowserRouter as Router} from "react-router-dom";
 function App() {
   return (
-    <h1>MY react app</h1>
+    <Router>
+      <Header/>
+     </Router>
+  
+
+
   );
 }
 
